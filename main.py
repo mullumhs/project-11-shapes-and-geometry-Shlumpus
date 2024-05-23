321e904ca16a62f969fdc200b26fdb3e19c72b3b
Shlumpus <140124184+Shlumpus@users.noreply.github.com>
2024-05-23 23:50:59 +0000

    setup shapes

---
+++ main.py
@@ -0,0 +1,19 @@
+class Shape:
+    def __init__(self, type):
+        self.type = type
+        
+    def calc_area():
+        print()
+        
+    def calc_perimeter():
+        print()
+        
+    
+class Square(Shape):
+    print()
+
+class Triangle(Shape):
+    print()
+
+class Circle(Shape):
+    print()
