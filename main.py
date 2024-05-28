b461be8a944163506e931b9a51ad79cf03fe9110
Shlumpus <140124184+Shlumpus@users.noreply.github.com>
2024-05-28 05:00:13 +0000

    Added the different shape's functions and adding user input to choose their shape

--- main.py
+++ main.py
@@ -1,19 +1,66 @@
+import math
+
 class Shape:
     def __init__(self, type):
         self.type = type
         
+    
+class Rectangle(Shape):
+    def __init__(self, type, width, height):
+        super.__init__(self, type)
+        width = self.width
+        height = self.height
+        
     def calc_area():
-        print()
+        area = width * height
+        print(area)
         
     def calc_perimeter():
-        print()
-        
-    
-class Square(Shape):
-    print()
+        perim = width * 2 + height * 2
+        print(perim)
 
 class Triangle(Shape):
-    print()
+    def __init__(self, type, base, length):
+        super.__init__(self, type)
+        
+        base = self.base
+        length = self.length
+        
+    def calc_area():
+        area = 0.5 * base * length
+        print(area)
+        
+    def calc_perimeter():
+        
 
 class Circle(Shape):
-    print()
+    def __init__(self, type, diameter):
+        super.__init__(self, type)
+
+        diameter = self.diameter
+        
+    def calc_area():
+        area = math.pi * (self.diameter / 2) ** 2
+
+print("Welcome to Shapes and Geometry!")
+
+choice = input("1. Rectangle\n2. Triangle\n3. Circle")
+
+if choice == "1":
+    width = int(input("Enter the width: "))
+    height = int(input("Enter the height: "))
+    rectangle1 = Rectangle(width, height)
+    print(f"\nRectangle: Area = {rectangle1.calc_area()}\nPerimeter = {rectangle1.calc_perimeter()}")
+
+if choice == 2:
+    base = int(input("Enter the triangle base: "))
+    length = int(input("Enter the triangle length: "))
+    triangle1 = Triangle(base, length)
+    print(f"\nTriangle: Area = {triangle1.calc_area()}\nPerimeter = {triangle1.calc_perimeter()}")
+    
+if choice == 3:
+    
+    
+
+
+    
